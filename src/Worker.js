2c5acec0f6c352b618b4463f0a28ea7ea0a57dbe
finscn <finscn@gmail.com>
2018-05-09 05:33:35 +0800

    terminate previous Worker automatically

--- src/Worker.js
+++ src/Worker.js
@@ -4,28 +4,43 @@ export default class Worker {
 
     constructor(file) {
 
-        this._file = file;
+        // 目前 微信小游戏中 Worker 最大并发数量限制为 1 个，
+        // 所以创建新Worker前, 需要结束现有的 Worker.terminate
 
-        this._worker = wx.createWorker(file);
+        if (Worker.previousWorker) {
+            Worker.previousWorker.terminate()
+        }
+        Worker.previousWorker = this
+
+        this._file = file
+
+        this._worker = wx.createWorker(file)
 
         this._worker.onMessage((res) => {
             if (this.onmessage) {
                 this.onmessage({
                     target: this,
                     data: res,
-                });
+                })
             }
-        });
+        })
 
     }
 
     postMessage(message, transferList) {
-        this._worker.postMessage(message, transferList);
+        this._worker.postMessage(message, transferList)
+    }
+
+    terminate() {
+        this._worker.terminate()
+        Worker.previousWorker = null
     }
 }
 
-// export default function(file) {
-//     const worker = wx.createWorker(file);
+Worker.previousWorker = null
 
-//     return worker;
-// };
+// export default function(file) {
+//     const worker = wx.createWorker(file)
+
+//     return worker
+// }
