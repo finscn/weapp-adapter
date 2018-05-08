a94935ddea30b8285c85ea18f9c3df7fd5b1e81e
finscn <finscn@gmail.com>
2018-05-09 00:10:31 +0800

    fix bug about Web Worker

--- src/Worker.js
+++ src/Worker.js
@@ -1,5 +1,31 @@
-export default function(file) {
-    const worker = wx.createWorker(file);
+export default class Worker {
 
-    return worker;
-};
+    onmessage = null
+
+    constructor(file) {
+
+        this._file = file;
+
+        this._worker = wx.createWorker(file);
+
+        this._worker.onMessage((res) => {
+            if (this.onmessage) {
+                this.onmessage({
+                    target: this,
+                    data: res,
+                });
+            }
+        });
+
+    }
+
+    postMessage(message, transferList) {
+        this._worker.postMessage(message, transferList);
+    }
+}
+
+// export default function(file) {
+//     const worker = wx.createWorker(file);
+
+//     return worker;
+// };
