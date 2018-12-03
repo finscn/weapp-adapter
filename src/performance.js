282f3ffbb2ff4278e852538928878a65f0554f42
finscn <finscn@gmail.com>
2018-12-03 16:17:53 +0800

    fix bug about `window.performance` in openDataContext

--- src/performance.js
+++ src/performance.js
@@ -2,27 +2,23 @@ let performance
 
 if (wx.getPerformance) {
     const { platform } = wx.getSystemInfoSync()
+    const wxPerf = wx.getPerformance()
+    const initTime = wxPerf.now()
 
-    if (wx.getPerformance) {
-        const wxPerf = wx.getPerformance()
-        const initTime = wxPerf.now()
+    const clientPerfAdapter = Object.assign({}, wxPerf, {
+        now: function() {
+            return (wxPerf.now() - initTime) / 1000
+        }
+    })
 
-        const clientPerfAdapter = Object.assign({}, wxPerf, {
-            now: function() {
-                return (wxPerf.now() - initTime) / 1000
-            }
-        })
-
-        performance = platform === 'devtools' ? wxPerf : clientPerfAdapter
-    } else {
-        performance = {
-            timeOrigin: Date.now(),
-            now: function() {
-                return Date.now() - this.timeOrigin
-            }
+    performance = platform === 'devtools' ? wxPerf : clientPerfAdapter
+} else {
+    performance = {
+        timeOrigin: Date.now(),
+        now: function() {
+            return Date.now() - this.timeOrigin
         }
     }
-
 }
 
 export default performance
